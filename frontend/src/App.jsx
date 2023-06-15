72f8a0394ca9aadeffdf032543d8330986d07054
@syabdulr <abdulsyed1998@gmail.com>
2023-06-15 11:01:48 -0400

    load 3 pages, with styles in vertical format

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+//App.jsx
 
+import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-// Note: Rendering a single component to build components in isolation
 const App = () => (
   <div className="App">
-    <PhotoListItem/>
+    {
+      Array.from({ length: 3 }).map((_, index) => <PhotoListItem key={index} />)
+    }
   </div>
-)
+);
 
-export default App
+export default App;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,10 +1,31 @@
+//PhotoListItem.jsx
 
-import React from 'react';
-
+import React, { useState } from 'react';
 import '../styles/PhotoListItem.scss';
+import FavIcon from './FavIcon';
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  const { id, location, imageSource, username, profile } = props;
+
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  const toggleFavorite = () => {
+    setIsFavorited(!isFavorited);
+  };
+  
+  return (
+    <div key={id} className="photo-list__item">
+      <img className="photo-list__image" src={imageSource} alt={`Photo by ${username}`} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={profile} alt={`Profile of ${username}`} />
+        <h2 className="photo-list__user-info">{username}</h2>
+        <p className="photo-list__user-location">{location.city}, {location.country}</p>
+      </div>
+      <div onClick={toggleFavorite}>
+        <FavIcon isFavorited={isFavorited} />
+      </div>
+    </div>
+  );
 }
 
 PhotoListItem.defaultProps = {
@@ -18,4 +39,4 @@ PhotoListItem.defaultProps = {
   "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
 }
 
-export default PhotoListItem
+export default PhotoListItem;
